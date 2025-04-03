ab9894b256d3160e9dd7933540660a9d1576af40
AmberAAA <anborong@outlook.com>
2025-04-03 16:03:06 +0800

    init

--- src/Button.tsx
+++ src/Button.tsx
@@ -8,6 +8,8 @@ const Button: FC<IButton> = (props) => {
   return <>
     <button onClick={props.onClick}>Button</button>
     <input aria-label="name" />
+    <input aria-label="age" />
+
   </>
 }
 
